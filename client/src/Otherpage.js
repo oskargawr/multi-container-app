e39926c711138677ac46aeb014d28c0da5c36aa3
oskargawr <oskargawryszewski1@gmail.com>
2024-04-03 10:18:38 +0200

    added client

---
+++ client/src/Otherpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Otherpage() {
+  return (
+    <div>
+        <h1>Other Page</h1>
+        <Link to="/">Go back to the main page</Link>
+    </div>
+  )
+}
+
+export default Otherpage
